fae604cbfb1fd7f40266d764890ef4ec5864c4a8
srivallioguri <86719087+srivallioguri@users.noreply.github.com>
2023-06-21 17:51:00 +0530

    Update CalculatorTest.java

--- src/test/java/program/sample/controller/CalculatorTest.java
+++ src/test/java/program/sample/controller/CalculatorTest.java
@@ -2,7 +2,7 @@ package program.sample.controller;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 public class ApplicationTest extends TestCase {
-    Calculator cal = new Calculator();
+    Calci cal = new Calci();()
 
     public void testAdd() {
         Assert.assertEquals(cal.add(10, 20), 30);
